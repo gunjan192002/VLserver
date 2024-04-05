c5cd903a1407a8b8e5158ade6ee3eb3a375f9b68
Gunjan Agrawal <gunjan11223374@gmail.com>
2024-04-06 01:09:58 +0530

    new

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 mongoose.connect("mongodb+srv://gunjan192002:gunjan1234@cluster0.1b3uuiv.mongodb.net/")
- app.get("/", (req, res) => {
-  res.send({ message: "Hello world"})
-  })
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+  });
 app.post('/login',(req,res) => {
 
     const {email,password}=req.body;
