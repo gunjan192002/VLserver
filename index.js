638cc7e5b64db792f1d4cbfdee172cb102ea57c4
Gunjan Agrawal <gunjan11223374@gmail.com>
2024-04-06 00:58:50 +0530

    jtrying new get request

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
-const express=require("express")
-const mongoose =require("mongoose")
-const cors=require("cors")
-const EmployeeModel=require('./models/Employee')
-const app=express()
-app.use(express.json())
-app.use(cors())
-mongoose.connect("mongodb+srv://gunjan192002:gunjan1234@cluster0.1b3uuiv.mongodb.net/")
- app.get("/", (req, res) => {
-  res.send({ message: "Hello world"})
-  })
-app.post('/login',(req,res) => {
+// 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-    const {email,password}=req.body;
-    EmployeeModel.findOne({email:email})
-    .then(user=>{
-        if(user){
-            if(user.password===password)
-            {
-                res.json("Success")
-                console.log("1")
-            }
-            else{
-                res.json("The password is incorrect")
-                console.log("2")
-            }
-        }
-        else{
-            res.json("No record existed")
-            console.log("3")
-        }
-    })
-})
-app.post('/register',(req,res) => {
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-    EmployeeModel.create(req.body)
-    .then(employees=>res.json(employees))
-    .catch(err=>console.log(err))
-})
-app.listen(3001,()=>{
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
 
-    console.log("server is running on port 3001")
+// GET request handler
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
 
-})
+// POST request handler
+app.post('/post-example', (req, res) => {
+  const { message } = req.body;
+  if (!message) {
+    return res.status(400).send('Message is required');
+  }
+  // Here you can process the received message
+  res.send(`Received message: ${message}`);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
